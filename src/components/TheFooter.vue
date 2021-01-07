be8b8eeb2f08d0f9b024c182a382a9d48ceb92c0
Akshay <akshayrajput102@gmail.com>
2021-01-07 18:55:10 +0530

    Add header and footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="">
-      Here is your footer
-  </div>
+  <footer class="text-center bg-gray-900 py-2">
+    <div class="container mx-auto">
+      &lt; / &gt; by 
+      <a href="https://akshayr.netlify.app" class="mx-1 text-green-500 hover:text-yellow-500" rel="noopener" target="_blank">Akshay</a>
+    </div>
+  </footer>
 </template>
 
 <script>
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="">
-      <div class="">
-          THis is your Navbar
+  <nav class="navbar mb-8 bg-gray-900">
+      <div class="py-2 container mx-auto ">
+        <router-link class="text-gray-500 hover:text-blue-700 hover:text-xl" tag="a" to="/">React.Jobs</router-link>
       </div>
   </nav>
 </template>
@@ -12,6 +12,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  // .navbar a{
+  //   background: #333;
+  // }
 </style>
