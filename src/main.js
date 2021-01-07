2bd57dc485c90d35d48cb05b833e5b5ba54c2a78
Akshay <akshayrajput102@gmail.com>
2021-01-07 17:46:01 +0530

    Import tailwind and maincss file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/tailwind.css'
+// import 'tailwindcss/tailwind.css'
+import './assets/main.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
