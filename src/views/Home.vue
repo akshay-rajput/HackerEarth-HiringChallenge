3d8f611b039ccf2ac1fed7a3c2eb0b9e291d5e85
Akshay <akshayrajput102@gmail.com>
2021-01-08 00:27:33 +0530

    import other components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <MyComponent msg="Sent prop to FirstComponent"/>
+  <div class="home mb-12">
+    <list-options-tabs @tabChange = "activeTab" :tablist = tabs></list-options-tabs>
+    <ListOfCandidates :allCandidates = all_candidates />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import MyComponent from '@/components/MyComponent.vue'
+import ListOptionsTabs from '@/components/ListOptionsTabs.vue'
+import ListOfCandidates from '@/components/ListOfCandidates.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return{
+      tabs: [
+        'All', 
+        'Shortlisted',
+        'Rejected'
+      ],
+      currentTab: 'All'
+    }
+  },
   components: {
-    MyComponent
+    ListOfCandidates,
+    ListOptionsTabs
+  },
+  mounted(){
+    this.$store.dispatch('action_fetchCandidates');  
+  },
+  computed: {
+    all_candidates(){
+      var list = this.$store.getters.getter_candidateList;
+      
+      if(this.currentTab == "Shortlisted"){
+        var shortlist = list.filter(item => {
+          return item.shortlisted == true;
+        })
+        return shortlist;  
+      }
+      else if(this.currentTab == "Rejected"){
+        var rejectedList = list.filter(item => {
+          return item.rejected == true;
+        })
+        return rejectedList;  
+      }
+      return this.$store.getters.getter_candidateList;
+    }
+  },
+  methods: {
+    activeTab: function(tabName){
+      this.currentTab = tabName;
+      console.log("tab change", this.currentTab);
+    }
   }
 }
 </script>
