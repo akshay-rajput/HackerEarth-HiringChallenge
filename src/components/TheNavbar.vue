60c4a20f15eb97a789d124939d87ccf17358af23
Akshay <akshayrajput102@gmail.com>
2021-01-08 00:27:46 +0530

    minor changes

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar mb-8 bg-gray-900">
-      <div class="py-2 container mx-auto ">
+      <div class="py-2 px-4 container mx-auto ">
         <router-link class="text-gray-500 hover:text-blue-700 hover:text-xl" tag="a" to="/">React.Jobs</router-link>
       </div>
   </nav>
